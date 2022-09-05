740c0965725ef89949da9d91a144bb2c7650f264
Nicholas <nc_chau@hotmail.com>
2022-09-05 18:16:05 +0000

    stores generated IDs upon POST request, redirects to urls/:id

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ function generateRandomString() {
     const randomIndex = Math.floor(Math.random() * charSet.length);
     newString += charSet[randomIndex];
   }
-  return newString;
+  return newString
 }
 
 const urlDatabase = {
@@ -30,8 +30,14 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+  res.redirect(`/urls/${newID}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
 });
 
 app.get("/urls/new", (req, res) => {
