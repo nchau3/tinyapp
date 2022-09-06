d5289b3261acf79bda43f47614ace6342dfcaf51
Nicholas <nc_chau@hotmail.com>
2022-09-06 13:34:22 +0000

    added routing for GET /register

--- express_server.js
+++ express_server.js
@@ -96,6 +96,15 @@ app.post("/logout", (req, res) => {
   res.redirect('/urls');
 });
 
+//go to register
+app.get("/register", (req, res) => {
+  const templateVars = { username: undefined };
+  if (req.cookies) {
+    templateVars.username = req.cookies["username"];
+  }
+  res.render("urls_register", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
