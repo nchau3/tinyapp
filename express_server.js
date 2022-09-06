4b479ff8d26e6e82c341d941969c55350ca054f8
Nicholas <nc_chau@hotmail.com>
2022-09-06 01:57:58 +0000

    added routing for logout, clears username from cookies

--- express_server.js
+++ express_server.js
@@ -90,6 +90,12 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+//logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
